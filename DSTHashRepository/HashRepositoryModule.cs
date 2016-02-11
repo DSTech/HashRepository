f4f863b9ebc3976ffe26737b5d6661b57073d6d8
Zoey <Dessix@Dessix.net>
2016-02-10 21:36:30 -0500

    Added arbitrary-directory support via LRU-cached hashing of the input paths.

--- DSTHashRepository/HashRepositoryModule.cs
+++ DSTHashRepository/HashRepositoryModule.cs
@@ -4,20 +4,45 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Nancy;
+using Caching;
 
 namespace DSTHashRepository {
 	public class HashRepositoryModule : Nancy.NancyModule {
+	    private LRUCache<string, string> HashCache = new LRUCache<string, string>(1024, 24);
 		private string ToHashName(string rawHash) => String.Concat(rawHash.Where(c => c != '/' && c != '.' && c != '\\'));//no navigation characters
 		private string FilesDir => Path.Combine(".", "files");
 		private string ToFilePath(string hashName) => Path.Combine(FilesDir, hashName);
-		private string ToHashPath(string rawHash) => ToFilePath(ToHashName(rawHash));
-		public HashRepositoryModule(IHashRepositoryConfig config) {
+
+        private string ToHashPath(string rawHash) {
+            string hex;
+            if (HashCache.TryGetValue(rawHash, out hex)) {
+                return hex;
+            }
+            byte[] hashHash;
+            using (var sha2 = new SHA256Managed()) {
+                hashHash = sha2.ComputeHash(Encoding.UTF8.GetBytes(rawHash));
+            }
+            hex = BytesToHex(hashHash);
+            HashCache.Add(rawHash, hex);
+            return hex;
+        }
+
+        public static string BytesToHex(byte[] bytes) {
+            var sb = new StringBuilder(bytes.Length * 2);
+            foreach (var b in bytes) {
+                sb.AppendFormat("{0:x2}", b);
+            }
+            return sb.ToString();
+        }
+
+        public HashRepositoryModule(IHashRepositoryConfig config) {
 			if (!Directory.Exists(FilesDir)) {
 				Directory.CreateDirectory(FilesDir);
 			}
 			Get["/"] = _ => "DST Hash Repository Prototype";
-			Head["/{hash:minLength(4)}"] = _ => {
+			Head["/{hash:minLength(4)*}"] = _ => {
 				var hashPath = ToHashPath((string)_.hash);
 				Console.WriteLine("HEAD: {0}", hashPath);
 				if (!File.Exists(hashPath)) {
@@ -25,7 +50,7 @@ namespace DSTHashRepository {
 				}
 				return HttpStatusCode.OK;
 			};
-			Get["/{hash:minLength(4)}"] = _ => {
+			Get["/{hash:minLength(4)*}"] = _ => {
 				var hashPath = ToHashPath((string)_.hash);
 				Console.WriteLine("GET: {0}", hashPath);
 				if (!File.Exists(hashPath)) {
@@ -33,7 +58,7 @@ namespace DSTHashRepository {
 				}
 				return Response.FromStream(File.OpenRead(hashPath), "application/binary");
 			};
-			Put["/{hash:minLength(4)}", true] = async (_, ct) => {
+			Put["/{hash:minLength(4)*}", true] = async (_, ct) => {
 				var hashPath = ToHashPath((string)_.hash);
 				var secretHeader = Request.Headers["secret"].FirstOrDefault();
 				if (!config.CheckCredentials("", secretHeader)) {
@@ -46,7 +71,7 @@ namespace DSTHashRepository {
 				}
 				return HttpStatusCode.OK;
 			};
-			Delete["/{hash:minLength(4)}"] = _ => {
+			Delete["/{hash:minLength(4)*}"] = _ => {
 				var hashPath = ToHashPath((string)_.hash);
 				var secretHeader = Request.Headers["secret"].FirstOrDefault();
 				if (!config.CheckCredentials("", secretHeader)) {
